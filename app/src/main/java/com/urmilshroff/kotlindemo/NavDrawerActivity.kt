41e2b14c6a4a12278dd737b1e153cc471c5a0da7
urmilshroff <urmilshroff@gmail.com>
2018-06-06 11:52:48 +0530

    Updated

--- app/src/main/java/com/urmilshroff/kotlindemo/NavDrawerActivity.kt
+++ app/src/main/java/com/urmilshroff/kotlindemo/NavDrawerActivity.kt
@@ -2,7 +2,6 @@ package com.urmilshroff.kotlindemo
 
 import android.os.Bundle
 import android.support.design.widget.NavigationView
-import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -11,17 +10,15 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_nav_drawer.*
 import kotlinx.android.synthetic.main.app_bar_nav_drawer.*
 
-class NavDrawerActivity:AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+class NavDrawerActivity:AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener
+{
 
-    override fun onCreate(savedInstanceState:Bundle?) {
+    override fun onCreate(savedInstanceState:Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_drawer)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener {view->
-            Snackbar.make(view,"Replace with your own action",Snackbar.LENGTH_LONG)
-                    .setAction("Action",null).show()
-        }
 
         val toggle=ActionBarDrawerToggle(
                 this,drawer_layout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close)
@@ -31,34 +28,43 @@ class NavDrawerActivity:AppCompatActivity(),NavigationView.OnNavigationItemSelec
         nav_view.setNavigationItemSelectedListener(this)
     }
 
-    override fun onBackPressed() {
-        if(drawer_layout.isDrawerOpen(GravityCompat.START)) {
+    override fun onBackPressed()
+    {
+        if(drawer_layout.isDrawerOpen(GravityCompat.START))
+        {
             drawer_layout.closeDrawer(GravityCompat.START)
         }
-        else {
+
+        else
+        {
             super.onBackPressed()
         }
     }
 
-    override fun onCreateOptionsMenu(menu:Menu):Boolean {
+    override fun onCreateOptionsMenu(menu:Menu):Boolean
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.nav_drawer,menu)
         return true
     }
 
-    override fun onOptionsItemSelected(item:MenuItem):Boolean {
+    override fun onOptionsItemSelected(item:MenuItem):Boolean
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        when(item.itemId) {
+        when(item.itemId)
+        {
             R.id.action_settings->return true
             else                ->return super.onOptionsItemSelected(item)
         }
     }
 
-    override fun onNavigationItemSelected(item:MenuItem):Boolean {
+    override fun onNavigationItemSelected(item:MenuItem):Boolean
+    {
         // Handle navigation view item clicks here.
-        when(item.itemId) {
+        when(item.itemId)
+        {
             R.id.nav_camera   -> {
                 // Handle the camera action
             }
