631e7e45307965f80c783e872c6d79fc68813c03
urmilshroff <urmilshroff@gmail.com>
2018-07-22 23:33:49 +0530

    Updated

--- app/src/main/java/com/urmilshroff/kotlindemo/LoginActivity.kt
+++ app/src/main/java/com/urmilshroff/kotlindemo/LoginActivity.kt
@@ -2,14 +2,25 @@ package com.urmilshroff.kotlindemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity:AppCompatActivity()
 {
-    //val usernameIn
+    lateinit var usernameInput:EditText
+    val button1=findViewById<Button>(R.id.button1)
 
     override fun onCreate(savedInstanceState:Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        button1?.setOnClickListener{onClick()}
+    }
+
+    private fun onClick()
+    {
+        Toast.makeText(this,"Hi",Toast.LENGTH_SHORT).show()
     }
 }
