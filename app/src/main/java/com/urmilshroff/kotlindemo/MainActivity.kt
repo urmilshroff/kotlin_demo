2b6ba7599b3466d0fce26c617fd042da017d7f70
urmilshroff <urmilshroff@gmail.com>
2018-08-01 00:42:07 +0530

    Updated

--- app/src/main/java/com/urmilshroff/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/urmilshroff/kotlindemo/MainActivity.kt
@@ -2,16 +2,20 @@ package com.urmilshroff.kotlindemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-
+import android.widget.TextView
 
 
 class MainActivity:AppCompatActivity()
 {
+    var helloMessage:TextView
 
     override fun onCreate(savedInstanceState:Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        helloMessage=findViewById(R.id.textView1)
+        helloMessage.setText("")
     }
 
 }
