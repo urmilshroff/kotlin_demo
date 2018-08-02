191aa660917183f6c62864d4a6b5e35c61179ba5
urmilshroff <urmilshroff@gmail.com>
2018-08-02 19:12:20 +0530

    Added setters and getters for camera shit

--- app/src/main/java/com/urmilshroff/kotlindemo/SharedPrefObj.kt
+++ app/src/main/java/com/urmilshroff/kotlindemo/SharedPrefObj.kt
@@ -9,12 +9,6 @@ object SharedPrefObj
     lateinit var sharedPrefVar:SharedPreferences
     lateinit var editor:SharedPreferences.Editor
 
-    fun getUsername(context:Context):String?
-    {
-        sharedPrefVar=PreferenceManager.getDefaultSharedPreferences(context)
-        return sharedPrefVar.getString("userName",null) //key value pair
-    }
-
     fun setUsername(context:Context,user:String?)
     {
         sharedPrefVar=PreferenceManager.getDefaultSharedPreferences(context)
@@ -24,12 +18,32 @@ object SharedPrefObj
         editor.commit()
     }
 
-    fun deleteData(context:Context)
+    fun getUsername(context:Context):String?
+    {
+        sharedPrefVar=PreferenceManager.getDefaultSharedPreferences(context)
+        return sharedPrefVar.getString("userName",null) //key value pair
+    }
+
+    fun destroyData(context:Context)
     {
         sharedPrefVar=PreferenceManager.getDefaultSharedPreferences(context)
         editor=sharedPrefVar.edit()
         editor.remove("userName")
         editor.clear()
         editor.commit()
+    }
+
+    fun setImage(context:Context,value:String)
+    {
+        sharedPrefVar=PreferenceManager.getDefaultSharedPreferences(context)
+        editor=sharedPrefVar.edit()
+        editor.putString("Image placeholder",value)
+        editor.commit()
+    }
+
+    fun getImage(context:Context):String?
+    {
+        sharedPrefVar=PreferenceManager.getDefaultSharedPreferences(context)
+        return sharedPrefVar.getString("") //fix
     }
 }
